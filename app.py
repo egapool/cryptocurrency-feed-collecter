212f10f53942bc26e839ee8cde6587f491538afd
egapool <egpersent@gmail.com>
2018-03-07 22:44:54 +0900

    公開日の文字列をdatetime型に

--- app.py
+++ app.py
@@ -10,6 +10,7 @@
 
 import feedparser
 import pymysql.cursors
+from datetime import datetime
 
 import html_parser
 import config
@@ -25,24 +26,28 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor)
 cursor = connection.cursor()
 
-for url in feeds.URLS:
-    d = feedparser.parse(url)
-    feeds = []
-    for item in d['entries']:
-        print(item['title'])
-        print(item['link'])
-        print(item['published'])
-        print(item['updated'])
-        print(item['content'][0]['value'][0:200])
-        body = html_parser.strip_tags(item['content'][0]['value'])[0:200]
-        
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO feeds (`title`,`url`,`body`) VALUES (%s,%s,%s)"
-            r = cursor.execute(sql, (item['title'],item['link'],body))
-            print(r) # -> 1
-            # autocommitではないので、明示的にコミットする
-            connection.commit()
-            
-connection.close()
+try:
+    for i in feeds.URLS:
+        print(i)
+        d = feedparser.parse(i['url'])
+        feeds = []
+        for item in d['entries']:
+            print(item['title'])
+            print(item['link'])
+            published = datetime.strptime(item['published'],i['tfmt']).strftime('%Y-%m-%d %H:%M:%S')
+            print(published)
+            print(item['updated'])
+    #         print(item['content'][0]['value'][0:200])
+            body = html_parser.strip_tags(item['content'][0]['value'])[0:200]
+
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO feeds (`title`,`url`,`body`,`published`) VALUES (%s,%s,%s,%s)"
+                r = cursor.execute(sql, (item['title'],item['link'],body,published))
+    connection.commit()
+except Exception as e:
+    connection.close()
+    print(e)
+else:
+    connection.close()
 
 
--- feeds.py
+++ feeds.py
@@ -1,5 +1,5 @@
 URLS = [
-    'https://medium.com/feed/@ShinichiroMatsuo/',
-    'http://www.digitalmoney.or.jp/feed/',
-    'http://doublehash.me/feed/'
+    {'url':'https://medium.com/feed/@ShinichiroMatsuo/','tfmt':'%a, %d %b %Y %H:%M:%S %Z'},
+    {'url':'http://www.digitalmoney.or.jp/feed/','tfmt':'%a, %d %b %Y %H:%M:%S %z'},
+    {'url':'http://doublehash.me/feed/','tfmt':'%a, %d %b %Y %H:%M:%S %z'},
 ]
