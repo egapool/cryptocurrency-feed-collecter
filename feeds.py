d4826b014aca4ceee82626d88a3f5147bf9a8467
harrypottercookies <101640185+harrypottercookies@users.noreply.github.com>
2022-03-24 15:43:52 +0200

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -2,4 +2,8 @@ URLS = [
     'https://medium.com/feed/@ShinichiroMatsuo/',
     'http://www.digitalmoney.or.jp/feed/',
     'http://doublehash.me/feed/',
+    'http://www.thecryptoquartet.com/feed/',
+    'https://www.bitdegree.org/crypto/news/rss',
+    'http://infoonbitcoin.com/feed/',
+    'https://7bitcoins.com/feed/,
 ]
